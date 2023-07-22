e6f6d736575b94416f843b2e1cfb8a1bd398d7b8
Kaki Gao <kaki.gao@coatesgroup.com>
2023-07-22 19:28:33 +0800

    Initial Commit

---
+++ KakiCodeios16/KakiCodeios16App.swift
@@ -0,0 +1,17 @@
+//
+//  KakiCodeios16App.swift
+//  KakiCodeios16
+//
+//  Created by Kaki on 2023/7/22.
+//
+
+import SwiftUI
+
+@main
+struct KakiCodeios16App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
