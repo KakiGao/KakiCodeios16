37938431ee8076e9d91fbe64816be739e4f90110
Kaki Gao <kaki.gao@coatesgroup.com>
2023-07-27 15:54:10 +0800

    Initial commit for SwiftUI

--- KakiCodeios16/ContentView.swift
+++ KakiCodeios16/ContentView.swift
@@ -9,13 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
+        VStack(alignment: .center, spacing: 20.0) {
+            Image(systemName: "timelapse", variableValue: 0.1)
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+                .font(.system(size: 50))
+                .fontWeight(.thin)
+            Text("Switching Apps")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+            Text("Tap and hold any part of the screen for 1 second to show the menu for switching between apps.")
+                .multilineTextAlignment(.center)
+            Button {
+                
+            }label: {
+                Text("Got it")
+                    .padding(.all)
+                    .frame(maxWidth: .infinity)
+                    .background(
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke()
+                    )
+            }
+            .accentColor(.black)
         }
-        .padding()
+        .padding(30)
+        .background(.regularMaterial)
+        .cornerRadius(10)
+        .overlay(RoundedRectangle(cornerRadius: 10).stroke())
+        .padding(10)
     }
 }
 
